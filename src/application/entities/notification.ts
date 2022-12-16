b3741f1d3e60c546fab907e47cda9e81becd6bfe
Bruno Simplicio <bruno@notabene.id>
2022-12-15 21:09:16 -0400

    Add UT for Content class

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,6 +1,8 @@
+import { Content } from './content';
+
 export interface NotificationProps {
   recipientId: string;
-  content: string;
+  content: Content;
   category: string;
   readAt?: Date | null;
   createdAt: Date;
@@ -20,6 +22,15 @@ export class Notification {
   public get recipientId(): string {
     return this.props.recipientId;
   }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
   public set category(category: string) {
     this.props.category = category;
   }
