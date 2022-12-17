f02ea0599bd846c13741671aa41be608542ff454
Bruno Simplicio <bruno@notabene.id>
2022-12-17 09:35:51 -0400

    Implementing prisma repo

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -11,5 +13,19 @@ export class PrismaNotificationMapper {
       canceledAt: notification.canceledAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw.canceledAt,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
